8ab02fac74a9a628bbd5b053fc0ba296418c6a52
AlexeyManankov <virtbrain@gmail.com>
2023-09-21 12:44:15 +0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Alexey Manankov on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
