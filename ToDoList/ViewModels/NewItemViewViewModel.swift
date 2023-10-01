2fcd427cd7b3c54c54f0066bce0340ae25e0420c
AlexeyManankov <virtbrain@gmail.com>
2023-10-01 19:41:07 +0500

    add save newToDo functionality

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Alexey Manankov on 29.09.2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -15,7 +17,35 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
-        //Save
+        guard canSave else {
+            return
+        }
+        
+        //Get current userId
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        //Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        //Save model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+        
+        
     }
     
     var canSave: Bool {
